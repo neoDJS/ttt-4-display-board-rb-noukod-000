d032d9c1789f44b03429567b89a16d781bc4d887
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-08-29 15:34:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,27 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def tab_line(l_num, tab)
+    line = " "
+	count = 0
+	3.times{ line  += "#{tab[ l_num*3 + count ]}"
+	         if count < 2
+			          line += " | "
+			     else
+			          line += " "
+			     end
+	         count += 1 }
+	return line
+end
+
+
+def display_board(board)
+  sLine = ""
+  count_l = 0
+
+  3.times{
+  	puts tab_line(count_l, board)
+  	if count_l < 2
+  		puts "-----------"
+  	end
+    count_l += 1
+  }
+
+end
